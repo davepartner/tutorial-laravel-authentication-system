3c283cc350157e871a19c860a27c5fb91abb5b91
Dave <daveozoalor@gmail.com>
2026-05-26 10:49:36 +0100

    style: further reduce auth logo size

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div class="w-full sm:max-w-md px-6">
                 <a href="/" class="block">
-                    <x-application-logo class="mx-auto h-auto w-full" />
+                    <x-application-logo class="mx-auto h-auto w-full max-w-xs" />
                 </a>
             </div>
 
